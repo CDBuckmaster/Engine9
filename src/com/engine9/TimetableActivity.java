0e2edaf97fba9a83a78f153133e2e2860837c8ed
YE Zhiyuan <tommyjackson2009@gmail.com>
2013-08-23 16:10:01 +1000

    Add some basic function in Timetable

--- src/com/engine9/TimetableActivity.java
+++ src/com/engine9/TimetableActivity.java
@@ -14,9 +14,45 @@ import android.widget.Button;
 
 public class TimetableActivity extends Activity {
 	private String vehicleID;
+	LinkedHashMap<String, List<String>> timetable = new LinkedHashMap();
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timetable);	
+	}
+	
+	private void addTimetable(String vehicleID, List<String> time) {
+		if (vehicleID == null || time == null || time.size() == 0) {
+			throw new NullPointerException();
+		}
+		timetable.put(vehicleID, time);
+	}
+	
+	public String toString() {
+		return timetable.toString();
+	}
+	
+	private void findTimetable(String s) {
+		if (s == null) {
+			throw new NullPointerException();
+		}
+		
+		//Find the vehicle timetable whether in the local store
+		if (!timetable.containsKey(s)) {
+			//Need to search the database can then display (Maybe store locally as well)
+		} else {
+			timetable.get(s);
+		}
+	}
+	
+	/***/
+	private void deleteTimetable(String s) {
+		if (s == null) {
+			throw new NullPointerException();
+		}
+		//Remove the timetable locally
+		if (timetable.containsKey(s)) {
+			timetable.remove(s);
+		} 
 	}
 }
