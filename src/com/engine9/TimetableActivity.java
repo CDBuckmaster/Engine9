df3668be47678667089debfa0cfa8656ab26be75
YE Zhiyuan <tommyjackson2009@gmail.com>
2013-08-24 14:29:16 +1000

    Add Json into Timetable

--- src/com/engine9/TimetableActivity.java
+++ src/com/engine9/TimetableActivity.java
@@ -1,8 +1,14 @@
 package com.engine9;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.io.*;
+import java.util.Date;
+
+import org.json.JSONObject;
 
 import com.engine9.R;
 import android.os.Bundle;
@@ -16,14 +22,15 @@ import android.widget.Button;
 public class TimetableActivity extends Activity {
 	private String vehicleID;
 	private LinkedHashMap<String, List<String>> timetable = new LinkedHashMap();
-	
-	private String time;
-	
+
+	private Date time;
+	private DateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timetable);	
 	}
-	
+
 	/**
 	 * Add the vehicle timetable to the local device*/
 	private void addTimetable(String vehicleID, List<String> time) {
@@ -32,18 +39,18 @@ public class TimetableActivity extends Activity {
 		}
 		timetable.put(vehicleID, time);
 	}
-	
+
 	public String toString() {
 		return timetable.toString();
 	}
-	
+
 	/**
 	 * Find the timetable for the vehicle based on stop*/
 	private void findStopTimetable(String stopID) {
 		if (stopID == null) {
 			throw new NullPointerException();
 		}
-		
+
 		//Find the vehicle timetable whether in the local store
 		if (!timetable.containsKey(stopID)) {
 			//Need to search the database can then display (Maybe store locally as well)
@@ -51,21 +58,21 @@ public class TimetableActivity extends Activity {
 			timetable.get(stopID);
 		}
 	}
-	
+
 	/**
 	 * Find the timetable for the vehicle based on service, structure same as stop timetable*/
 	private void findServiceTimetable(String serviceID) {
 		if (serviceID == null) {
 			throw new NullPointerException();
 		}
-		
+
 		if (!timetable.containsKey(serviceID)) {
-			
+
 		} else {
 			timetable.get(serviceID);
 		}
 	}
-	
+
 	/**
 	 * Delete the timetable from local device*/
 	private void deleteStopTimetable(String timetableKey) {
@@ -77,23 +84,30 @@ public class TimetableActivity extends Activity {
 			timetable.remove(timetableKey);
 		} 
 	}
-	
+
 	/**
 	 * Count the time and highlight the service if approaching within 5 mim*/
 	private void timeCountDown() {
-		
+
 	}
-	
+
 	/**
-	 * Get the vehicle data from database*/
-	private void getVehicle() {
-		
+	 * Set which type of data will be display*/
+	private void dataDisplay() {
+
 	}
-	
+
 	/**
 	 * Check the timetable data whether is service realtime or static. If the 
 	 * realtime data can be find, use that; show the static data otherwise*/
 	private void checkDataType() {
-		
+
+	}
+
+	private class TimeRequest extends Request{
+		@Override
+		public void onPostExecute(String result) {
+			JSONObject jData = new JSONObject((Map) JParser.pObject(result));
+		}
 	}
 }
