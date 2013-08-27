24212228e96b816fcce7e844e9c4b7d120b6587f
CDBuckmaster <callumbuckmaster@gmail.com>
2013-08-27 15:20:54 +1000

    Added FavouriteManager and basic layout

---
+++ src/com/engine9/FavouriteActivity.java
@@ -0,0 +1,11 @@
+package com.engine9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FavouriteActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ src/com/engine9/FavouriteManager.java
@@ -0,0 +1,70 @@
+package com.engine9;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+import android.content.Context;
+
+public class FavouriteManager {
+
+	public static void AddFavourite(String fav, Context context){
+		FileOutputStream outputStream;
+		try {
+			outputStream =context.openFileOutput("favourites.txt", Context.MODE_APPEND);
+			outputStream.write((fav + '\n').getBytes());
+			outputStream.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public static void deleteFavourite(String fav, Context context){
+		try {
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					context.openFileInput("favourites.txt")));
+			String inputStr;
+			StringBuffer stringBuffer = new StringBuffer();
+			while((inputStr = input.readLine()) !=  null){
+				if(inputStr != fav){
+					stringBuffer.append(inputStr + '\n');
+				}
+			}
+			input.close();
+			
+			FileOutputStream outputStream;
+			outputStream =context.openFileOutput("favourites.txt", Context.MODE_PRIVATE);
+			outputStream.close();
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Vector<String> getFavourites(Context context){
+		
+		try{
+			Vector<String> retVector = new Vector<String>();
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					context.openFileInput("favourites.txt")));
+			String inputStr;
+			while((inputStr = input.readLine()) !=  null){
+				retVector.add(inputStr);
+			}
+			
+			return retVector;
+		} catch(Exception e){
+			return null;
+		}
+	}
+	
+	
+}
